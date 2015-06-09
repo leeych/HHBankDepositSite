5b3709974de95e7dd5947f286144692d67289216
leeych <leeych@users.noreply.github.com>
2015-06-09 12:51:07 +0800

    新增“退出登录”界面，机构信息显示功能。

---
+++ model/OrgInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class OrgInfo
+    {
+        public string OrgCode { get; set; }
+        public string OrgName { get; set; }
+        public string OrgAddress { get; set; }
+        public string OrgPhone { get; set; }
+    }
+}
